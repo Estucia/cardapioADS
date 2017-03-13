600545aeedca767e186a9089a199fee689de6c78
Estucia <germandeveloperr@gmail.com>
2017-03-13 20:59:17 -0300

    aaa

---
+++ js/app.js
@@ -0,0 +1,12 @@
+angular.module('cardapioADSApp',['ngRoute'])
+
+.config(function($routeProvider){
+    
+    $routeProvider.when('/', {
+        templateUrl: 'view/login.html',
+        controller: 'loginCtrl'
+    });
+
+    $routeProvider.otherwise({redirectTo: '/'})
+
+});
