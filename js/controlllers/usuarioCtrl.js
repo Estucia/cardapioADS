68dca3276ab8650fac6178fbc806628a6ae625b3
Estucia <germandeveloperr@gmail.com>
2017-03-29 22:48:02 -0300

    29/03/17

--- js/controlllers/usuarioCtrl.js
+++ js/controlllers/usuarioCtrl.js
@@ -2,4 +2,19 @@ angular.module('cardapioADSApp')
 
 .controller('usuarioCtrl',function($scope) {
 
+    $scope.usuario = {
+        id: null,
+        nome: null,
+        login: null,
+        senha: null
+    }
+
+    $scope.listaUsuarios = [];
+
+    $scope.salvar = function(usuarioCadastrado){
+        $scope.listaUsuarios.push(usuarioCadastrado);
+        delete $scope.usuario;
+        $('#cadastroUsuariosModal').modal('hide');
+    }
+
 });
